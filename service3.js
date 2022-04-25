33ebcbe852e09e3cd2ecf6df24fc9bf027bd2e17
taritinth <tarit.khanbo@hotmail.com>
2022-04-25 17:58:48 +0700

    init:

---
+++ service3.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const port = 3002;
+
+const { PrometheusExporter } = require("@opentelemetry/exporter-prometheus");
+const { MeterProvider } = require("@opentelemetry/sdk-metrics-base");
+
+// Add your port and startServer to the Prometheus options
+const options = { port: 9464, startServer: true };
+const exporter = new PrometheusExporter(options);
+
+// Register the exporter
+const meter = new MeterProvider({
+  exporter,
+  interval: 1000,
+}).getMeter("demo-prometheus");
+
+// Now, start recording data
+const counter = meter.createCounter("62070087_api_count", {
+  description: "Count movies api",
+});
+
+app.get("/movies", async function (req, res) {
+  counter.add(1, { service_name: "service_3" });
+
+  res.type("json");
+  var delay = Math.floor(Math.random() * 2000 + 100);
+
+  setTimeout(() => {
+    res.send({
+      movies: [
+        { name: "Jaws", genre: "Thriller" },
+        { name: "Annie", genre: "Family" },
+        { name: "Jurassic Park", genre: "Action" },
+      ],
+    });
+  }, delay);
+});
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
